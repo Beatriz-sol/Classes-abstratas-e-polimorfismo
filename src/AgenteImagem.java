8b07969b67618d8ea23195cfba7d7d5c0c3c381f
Beatriz-sol <10443916@mackenzista.com.br>
2026-08-21 12:52:44 -0300

    adiciona AgenteImagem e Safety Guard

---
+++ src/AgenteImagem.java
@@ -0,0 +1,29 @@
+public class AgenteImagem extends AgenteIA {
+
+    public AgenteImagem(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void processarRequisicao(String input)
+            throws FalhaProcessamentoAgenteException,
+                   PromptInadequadoException,
+                   ErroComunicacaoIAException {
+
+        conectarServidor();
+
+        if (input == null || input.length() == 0) {
+            throw new FalhaProcessamentoAgenteException("O prompt não pode estar vazio.");
+        }
+
+        String promptMinusculo = input.toLowerCase();
+
+        if (promptMinusculo.contains("hackear")|| promptMinusculo.contains("roubar")|| promptMinusculo.contains("biométrico")) {
+
+            throw new PromptInadequadoException("Prompt bloqueado pelo Safety Guard.");
+        }
+
+        System.out.println(
+            "Agente de Imagem " + getNome() +" sintetizando pixels para: " + input);
+    }
+}
