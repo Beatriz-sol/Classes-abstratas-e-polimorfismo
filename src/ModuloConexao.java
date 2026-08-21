0caf4d003f15e665234032bdd136d807f4df835d
Beatriz-sol <10443916@mackenzista.com.br>
2026-08-21 18:05:16 -0300

    metodo e regra

--- src/ModuloConexao.java
+++ src/ModuloConexao.java
@@ -4,3 +4,16 @@ public class ModuloConexao {
             throws ErroComunicacaoIAException {
 
         double numero = Math.random();
+
+        if (numero > 0.8) {
+
+            throw new ErroComunicacaoIAException(
+                "Falha na comunicação com a GPU."
+            );
+        }
+
+        System.out.println(
+            "Conexão estabelecida com sucesso."
+        );
+    }
+}
